8e0df30a7d436e38a8a66f0dfe94625374c991cd
wurbuzz <mitchell.burt@hotmail.com>
2024-12-05 11:23:58 -0500

    Fixes

--- displaySpecies.js
+++ displaySpecies.js
@@ -7,7 +7,7 @@ function appendSpeciesToTable(speciesName){
     if(species[speciesName]["ID"] >= 420){
         return false
     }
-	    if(species[speciesName]["name"] == "Zygarde"){
+	if(species[speciesName]["name"] == "Zygarde"){
         return false
     }
 	
